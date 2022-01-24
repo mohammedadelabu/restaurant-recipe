8f974e0415ddf507481ae8e1a4ba9892972016bf
Emperor Hassy <decagon@Emperor-Hassys-MacBook-Air.local>
2022-01-24 15:05:18 +0100

    FIXED THE EJS PART

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../public')));
+app.use(express.static(path.join(__dirname, '../public/css')));
 
 app.use('/api/auth', authRoute);
 app.use('/api/recipes', recipeRoute);
